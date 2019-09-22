0a0546c3bda23d2625c119e649ab8a3442216556
SangYeon Park <psyssai@gmail.com>
2019-09-22 16:45:25 +0900

    3.DataFrame Add

--- Build.bat
+++ Build.bat
@@ -1,5 +1,9 @@
 
-jupyter nbconvert --to markdown C:\Users\82103\PandasBlogPosting\PandasBasic_1_Series.ipynb --output d:\PSYda\content\blog\BigData\pandas\[Pandas기초]2.Pandas의자료구조_Series.md
+jupyter nbconvert --to markdown C:\Users\82103\PandasBlogPosting\PandasBasic_1_Series.ipynb --output d:\PSYda\content\blog\BigData\pandas\2_Series.md
+jupyter nbconvert --to markdown C:\Users\82103\PandasBlogPosting\PandasBasic_2_DataFrame.ipynb --output d:\PSYda\content\blog\BigData\pandas\3_DataFrame.md
+
+
+jupyter nbconvert --to markdown C:\Users\82103\PandasBlogPosting\PandasBasic_3_DataFrame_Merge.ipynb --output d:\PSYda\content\blog\BigData\pandas\4_DataFrame_Merge.md
 
 
 hugo -t hugo-future-imperfect
