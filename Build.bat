801e7e1ce91a5a2fc24dfd5229590a3880801390
SangYeon Park <psyssai@gmail.com>
2019-09-24 23:10:36 +0900

    8.DataFrame sort and rank function Add

--- Build.bat
+++ Build.bat
@@ -1,11 +1,18 @@
 
-jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_1_Series.ipynb --output /d/PSYda/content/blog/BigData/pandas/2_Series.md
-jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_2_DataFrame.ipynb --output /d/PSYda/content/blog/BigData/pandas/3_DataFrame.md
-sed -i 's/img\//\/img\//g' /d/PSYda/content/blog/BigData/pandas/3_DataFrame.md
-jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_3_DataFrame_Merge.ipynb --output /d/PSYda/content/blog/BigData/pandas/4_DataFrame_Merge.md
-sed -i 's/img\//\/img\//g' /d/PSYda/content/blog/BigData/pandas/4_DataFrame_Merge.md
-jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_4_DataFrame_FileIO.ipynb --output /d/PSYda/content/blog/BigData/pandas/5_dataframe_fileio.md
-jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_5_DataFrame_ArithmeticOperation.ipynb --output /d/PSYda/content/blog/BigData/pandas/6_dataframe_calc.md
-sed -i 's/img\//\/img\//g' /d/PSYda/content/blog/BigData/pandas/6_dataframe_calc.md
-jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_6_DataFrame_Statistic.ipynb --output /d/PSYda/content/blog/BigData/pandas/7_dataframe_statistic.md
-sed -i 's/D\%3A\/PSYda\/content\/\//g' /d/PSYda/content/blog/BigData/pandas/6_dataframe_calc.md
+jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_1_Series.ipynb --output /d/PSYda/content/blog/bigdata/pandas/2_Series.md
+
+jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_2_DataFrame.ipynb --output /d/PSYda/content/blog/bigdata/pandas/3_DataFrame.md
+sed -i 's/img\//\/img\//g' /d/PSYda/content/blog/bigdata/pandas/3_DataFrame.md
+
+jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_3_DataFrame_Merge.ipynb --output /d/PSYda/content/blog/bigdata/pandas/4_DataFrame_Merge.md
+sed -i 's/img\//\/img\//g' /d/PSYda/content/blog/bigdata/pandas/4_DataFrame_Merge.md
+
+jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_4_DataFrame_FileIO.ipynb --output /d/PSYda/content/blog/bigdata/pandas/5_dataframe_fileio.md
+
+jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_5_DataFrame_ArithmeticOperation.ipynb --output /d/PSYda/content/blog/bigdata/pandas/6_dataframe_calc.md
+sed -i 's/img\//\/img\//g' /d/PSYda/content/blog/bigdata/pandas/6_dataframe_calc.md
+
+jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_6_DataFrame_Statistic.ipynb --output /d/PSYda/content/blog/bigdata/pandas/7_dataframe_statistic.md
+sed -i 's/D\%3A\/PSYda\/content\//\//g' /d/PSYda/content/blog/bigdata/pandas/7_dataframe_statistic.md
+
+jupyter nbconvert --to markdown /C/Users/82103/PandasBlogPosting/PandasBasic_7_DataFrame_sort_rank.ipynb --output /d/PSYda/content/blog/bigdata/pandas/8_dataframe_sortrank.md
